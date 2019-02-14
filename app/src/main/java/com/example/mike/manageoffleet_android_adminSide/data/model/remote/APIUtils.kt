721d565e47ddb9248120333f1770998b8827c7e2
Michalis Chaviaras <michav1510@gmail.com>
2019-02-14 09:34:26 +0200

    first commit

--- app/src/main/java/com/example/mike/manageoffleet_android_adminSide/data/model/remote/APIUtils.kt
+++ app/src/main/java/com/example/mike/manageoffleet_android_adminSide/data/model/remote/APIUtils.kt
@@ -2,7 +2,7 @@ package com.example.mike.manageoffleet_android_adminSide.data.model.remote
 
 object ApiUtils {
 
-    val BASE_URL = "https://havistudio.com/MonitoringOfPhones/"
+    val BASE_URL = "-----------------------------"
 
     val apiService: APIService
         get() = RetrofitClient.getClient(BASE_URL)!!.create(APIService::class.java)
